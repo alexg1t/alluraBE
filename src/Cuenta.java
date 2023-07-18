dbd53d22497a68f1806dae46cc2dee9b3bd1cd5b
Alexander <alexanderfromhell4@gmail.com>
2023-07-18 15:26:57 -0500

    Test referencias entre objetos

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -2,7 +2,7 @@ class Cuenta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular;
 
    public void depositar(double valor){
         this.saldo = this.saldo+valor;
---
+++ src/TestReferencia2.java
@@ -0,0 +1,14 @@
+public class TestReferencia2 {
+    public static void main(String[] args){
+        Cliente diego= new Cliente();
+        diego.nombre = "Diego";
+        diego.documento = "323232";
+        diego.telefono="90232323";
+
+        Cuenta cuentaDiego = new Cuenta();
+        cuentaDiego.agencia = 1;
+        cuentaDiego.titular = diego;
+
+        System.out.println(cuentaDiego.titular.documento);
+    }
+}
